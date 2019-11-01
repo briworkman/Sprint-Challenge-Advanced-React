b8375a4d4bda4d6e63874628fb0ae385bc7478ba
Brianna Workman <brianna.workman@hotmail.com>
2019-11-01 10:12:52 -0500

    Pulls data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(response => {
+        console.log(response);
+        this.setState({
+          data: response.data
+        });
+      })
+      .catch(err => {
+        console.log("Could not fetch data", err);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Sprint Practice</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
