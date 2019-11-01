c485021b6088c21acb0280061a1b46c1319fbb3c
Brianna Workman <brianna.workman@hotmail.com>
2019-11-01 12:09:50 -0500

    Adds Styling

--- client/src/components/PlayerStats.js
+++ client/src/components/PlayerStats.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function PlayerStats(props) {
   return (
-    <div>
-      <h3>Name: {props.name}</h3>
-      <p>Country: {props.country}</p>
-      <p>Searches: {props.searches}</p>
+    <div className="card-container">
+      <div className="player-card">
+        <h3 className="player-name">Name: {props.name}</h3>
+        <p>Country: {props.country}</p>
+        <p>Searches: {props.searches}</p>
+      </div>
     </div>
   );
 }
